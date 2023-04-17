9c8e8c054e5a3bb818034d671511dc60285e1f36
xiewen <1123654054@qq.com>
2023-04-17 09:25:40 +0800

    使用useRef完成过度动画，防止内存泄漏

--- MyData/react-count/src/layout/WelcomeLayout.tsx
+++ MyData/react-count/src/layout/WelcomeLayout.tsx
@@ -1,25 +1,27 @@
 import { animated, useTransition } from "@react-spring/web";
 import * as React from "react";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 import { useLocation, useOutlet } from "react-router-dom";
-const map: Record<string, ReactNode> = {};
+// const map: Record<string, ReactNode> = {}; //这里使用map会有内存泄漏，当路由不在是welcome，WelcomeLayout组件会销毁，但是map还在js内存中
 export const WelcomeLayout: React.FC = () => {
   const location = useLocation();
   const outlet = useOutlet();
-  map[location.pathname] = outlet;
-  console.log(map, "123");
-
+  const map = useRef<Record<string, ReactNode>>({});
+  map.current[location.pathname] = outlet;
   const transitions = useTransition(location.pathname, {
-    from: { transform: "translateX(100%)" },
+    from: {
+      transform:
+        location.pathname === "/welcome/1"
+          ? "translateX(0%)"
+          : "translateX(100%)",
+    },
     enter: { transform: "translateX(0%)" },
     leave: { transform: "translateX(-100%)" },
-    config: { duration: 10000 },
+    config: { duration: 1000 },
   });
-  return transitions((style, pathname) => {
-    return (
-      <animated.div key={pathname} style={style}>
-        <div style={{ textAlign: "left" }}>{map[pathname]}</div>
-      </animated.div>
-    );
-  });
+  return transitions((style, pathname) => (
+    <animated.div key={pathname} style={style}>
+      <div style={{ textAlign: "left" }}>{map.current[pathname]}</div>
+    </animated.div>
+  ));
 };
