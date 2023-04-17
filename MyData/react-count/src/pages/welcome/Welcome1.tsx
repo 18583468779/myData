e3d9c7644eb2c3be72bcdea902e01673249222bc
xiewen <1123654054@qq.com>
2023-04-17 09:48:16 +0800

    配置unocss以及添加shims.d.ts文件

--- MyData/react-count/src/pages/welcome/Welcome1.tsx
+++ MyData/react-count/src/pages/welcome/Welcome1.tsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 export const Welcome1: React.FC = () => {
   return (
     <div style={{ border: "1px solid #ccc", width: "100%" }}>
-      <div>Welcome1 </div>
+      <div text-24px>Welcome1 </div>
+      <div flex>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
       <Link to={"/welcome/2"}>下一页</Link>
     </div>
   );
