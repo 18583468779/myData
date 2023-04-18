d29255978ad951be373855a87d5ed43e7b78c316
xiewen <1123654054@qq.com>
2023-04-18 16:32:45 +0800

    引入mock

---
+++ MyData/react-count/mock/test.ts
@@ -0,0 +1,4 @@
+import { MockMethod } from "vite-plugin-mock";
+import { meMock } from "./me.mock";
+
+export default [...meMock] as MockMethod[];
--- MyData/react-count/vite.config.ts
+++ MyData/react-count/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteMockServe } from "vite-plugin-mock";
 import Unocss from "unocss/vite";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [Unocss({}), react()],
+export default defineConfig((env) => {
+  const { command } = env;
+
+  return {
+    define: {
+      isDev: command === "serve",
+    },
+    plugins: [viteMockServe(), Unocss({}), react()],
+  };
 });
