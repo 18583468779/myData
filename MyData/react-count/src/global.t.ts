b8c4186576fb11f125408c94e6914c521c66aa62
xiewen <1123654054@qq.com>
2023-04-19 16:30:25 +0800

    itemsPage基本样式完成

--- MyData/react-count/src/global.t.ts
+++ MyData/react-count/src/global.t.ts
@@ -9,3 +9,15 @@ type User = {
   created_at: string;
   updated_at: string;
 };
+interface Item {
+  id: number;
+  user_id: number;
+  amount: number;
+  note?: string;
+  tag_ids: number[];
+  happen_at: string;
+  created_at: string;
+  updated_at: string;
+  kind: "expenses" | "incomes";
+  deleted_at?: string;
+}
--- MyData/react-count/src/pages/ItemPage.tsx
+++ MyData/react-count/src/pages/ItemPage.tsx
@@ -13,6 +13,28 @@ export const ItemPage: React.FC = () => {
   const Div = styled.div`
     background: linear-gradient(0deg, #12b696 0%, #56ca71 100%);
   `;
+  const [items] = useState<Item[]>([
+    {
+      id: 1,
+      kind: "incomes",
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: "2021-01-01T00:00:00.000Z",
+      created_at: "2021-01-01T00:00:00.000Z",
+      updated_at: "2021-01-01T00:00:00.000Z",
+    },
+    {
+      id: 2,
+      kind: "incomes",
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: "2021-01-01T00:00:00.000Z",
+      created_at: "2021-01-01T00:00:00.000Z",
+      updated_at: "2021-01-01T00:00:00.000Z",
+    },
+  ]);
 
   const [selected, setSelected] = useState<TimeRange>("thisMouth"); //默认的本月
   return (
@@ -22,7 +44,7 @@ export const ItemPage: React.FC = () => {
         <TimeRangePicker selected={selected} setSelected={setSelected} />
       </Div>
       <ItemsSummary />
-      <ItemsList />
+      <ItemsList items={items} />
       <AddItemPage />
     </div>
   );
